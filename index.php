2b492087d25051f8807af06518c6f498b839c231
timegold-websrc <39736627+timegold-websrc@users.noreply.github.com>
2018-06-07 21:38:46 +0800

    macro function

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once './vendor/autoload.php';
 $loader = new Twig_Loader_Filesystem('sample');
 $twig = new Twig_Environment($loader);
 
-echo $twig->render('hello.twig', array(
+echo $twig->render('macro.twig', array(
     'name' => 'Time',
     'age' => 32
 ));
